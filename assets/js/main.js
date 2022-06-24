6ea50484228574bf681a0db1743af08e65821d63
heykei <trupin.kenny@gmail.com>
2022-06-24 12:44:20 +0200

    test_out_lettre_input

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,16 +17,45 @@ crypto.onblur = function(){
     flip.style.transform = "rotateY(0deg)";
 }
 
+function verifierCaracteres(event) {
+             
+    var keyCode = event.which ? event.which : event.keyCode;
+    var touche = String.fromCharCode(keyCode);
+            
+    var champ = document.getElementById('num_card'); 
+    var caracteres = '0123456789';
+            
+    if(caracteres.indexOf(touche) >= 0) {
+        champ.value += touche;
+    }
 
+    if (num_card.value.length === 4 || num_card.value.length === 9 || num_card.value.length === 14) {
+        num_card.value +=" ";
+    }
+    else {
+        return num_card;
+    }      
+}
 
+// function onlyNumber()
+// {
+//     var num_card = document.getElementById('num_card');
+//     if (num_card.value.match(/[^0-9]/,''))
+//     {
+//         num_card.value = num_card.value.replace(/[^0-9]/,' ');
+//     }
+//     if (num_card.value.length === 4 || num_card.value.length === 9 || num_card.value.length === 14) {
+//         num_card.value +=" ";
+//     }
+// };
 
 num_card.addEventListener('input', (e) => {
     
     const value = num_card.value;
-    if (num_card.value.length === 0){
+    if (num_card.value.length == 0){
     contenu.innerHTML ='<p>&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;</p>';
     }
-    if (num_card.value.length === 1 || num_card.value.length === 2 || num_card.value.length === 3 || num_card.value.length === 4 ||
+    if (num_card.value.length == 1 || num_card.value.length == 2 || num_card.value.length === 3 || num_card.value.length === 4 ||
         num_card.value.length === 5 || num_card.value.length === 6 || num_card.value.length === 7 || num_card.value.length === 8 ||
         num_card.value.length === 9 || num_card.value.length === 10 || num_card.value.length === 11 || num_card.value.length === 12 ||
         num_card.value.length === 13 || num_card.value.length === 14 || num_card.value.length === 15 || num_card.value.length === 16 ||
@@ -35,6 +64,7 @@ num_card.addEventListener('input', (e) => {
         contenu.innerHTML =`${value}`;
         }
 });
+
 
 name_card.addEventListener('input', (e) => {
     
@@ -46,7 +76,8 @@ name_card.addEventListener('input', (e) => {
         name_card.value.length === 5 || name_card.value.length === 6 || name_card.value.length === 7 || name_card.value.length === 8 ||
         name_card.value.length === 9 || name_card.value.length === 10 || name_card.value.length === 11 || name_card.value.length === 12 ||
         name_card.value.length === 13 || name_card.value.length === 14 || name_card.value.length === 15 || name_card.value.length === 16 ||
-        name_card.value.length === 17 || name_card.value.length === 18 || name_card.value.length === 19 || name_card.value.length === 20 
+        name_card.value.length === 17 || name_card.value.length === 18 || name_card.value.length === 19 || name_card.value.length === 20 ||
+        name_card.value.length === 21 || name_card.value.length === 22 || name_card.value.length === 23 || name_card.value.length === 24
         ){
         contenu2.innerHTML =`${value}`;
         }
